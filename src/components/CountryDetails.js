89d1d532cf5543b2c27bd66a9434b14c4111a285
Carl Berthold <Carl@space-time.tv>
2021-09-23 19:31:34 +0200

    CountryList

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import countriesList from "../countries.json"
+
+function CountryDetails() {
+
+    // Countries.find() country finden bei id ? 
+    // const foundCountry = countriesList.find((theCountry) => {
+    //     console.log(theCountry)
+    //     console.log(foundCountry)
+    //   });
+
+      return (
+        <div>
+        <h1>hello</h1>
+        </div>
+      )
+}      
+
+export default CountryDetails;
